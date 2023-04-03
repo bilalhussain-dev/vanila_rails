936342d69a477363c7c773dd8d03e95407024c90
Bilal Hussain <bilalhussian785@gmail.com>
2023-04-03 14:38:17 +0500

    adding validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  
+  before_save {self.email = email.downcase}
+  validates :username, presence: true, length: {minimum: 5, maximum: 30}
+  validates :email, presence: true, uniqueness: true , length: {minimum: 5, maximum: 30},
+            format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
 end
