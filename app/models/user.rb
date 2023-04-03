d8c28d0339e45e08d5f4831c3b54de32c65e0d12
Bilal Hussain <bilalhussian785@gmail.com>
2023-04-03 17:37:33 +0500

    add secure password to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ActiveRecord::Base
   validates :username, presence: true, length: {minimum: 5, maximum: 30}
   validates :email, presence: true, uniqueness: true , length: {minimum: 5, maximum: 30},
             format: {with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i}
+
+
+  has_secure_password
 end
