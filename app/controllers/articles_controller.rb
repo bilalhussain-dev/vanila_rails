8d009afbe0c590e2a7d1024c22444d362baf84d7
Bilal Hussain <bilalhussian785@gmail.com>
2023-04-03 11:04:12 +0500

    modified app crud operations

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,5 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :destroy, :update]
   def index
     @articles = Article.all
   end
@@ -19,15 +20,12 @@ class ArticlesController < ApplicationController
 
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "Article Updated"
       redirect_to article_path(@article)
@@ -35,8 +33,19 @@ class ArticlesController < ApplicationController
       render 'edit', status: :unprocessable_entity
     end
   end
+
+  def destroy
+    @article.destroy
+    flash[:notice] = "Article Deleted Successfully"
+    redirect_to articles_path
+  end
+
   private
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+
+  def set_article
+    @article = Article.find(params[:id])
   end
 end
