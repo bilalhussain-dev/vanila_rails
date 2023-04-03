8e553b744f5be26cb32d0d7e802d7b7f14a7b07c
Bilal Hussain <bilalhussian785@gmail.com>
2023-04-03 19:36:57 +0500

    User update settins

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "Account Updated Successfully"
+      redirect_to articles_path
+    else
+      render :edit
+    end
+  end
+
 
   private
   def user_params
