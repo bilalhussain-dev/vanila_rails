a3a05573ea71730207627f6f3923a7933fbbc567
Bilal Hussain <bilalhussian785@gmail.com>
2023-04-03 15:20:00 +0500

    add association to article and user

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ActiveRecord::Base
+  belongs_to :user
   validates :title, presence: true, length: {minimum:5, maximum: 200}
   validates :description, presence: true, length: {minimum: 5, maximum: 2000}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :articles
   before_save {self.email = email.downcase}
   validates :username, presence: true, length: {minimum: 5, maximum: 30}
   validates :email, presence: true, uniqueness: true , length: {minimum: 5, maximum: 30},
